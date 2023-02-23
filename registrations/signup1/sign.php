f4a614d1e9721e23d27351d82a1156f91340117f
aaronkd <126169454+aaronkd@users.noreply.github.com>
2023-02-23 21:14:01 +0800

    Add files via upload
    Must add phpMyAdmin database with name "signupforms" in order to work properly

---
+++ registrations/signup1/sign.php
@@ -0,0 +1,93 @@
+<?php
+$success=0;
+$user=0;
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    include 'connect.php';
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // $sql = "insert into `registration` (username, password) values('$username', '$password')";
+    // $result=mysqli_query($con,$sql);
+
+    // if ($result) {
+    //     echo "Data inserted succesfully";
+    // } else {
+    //     die(mysqli_error($con));
+    // }
+
+    $sql = "Select * from `registration` where username = '$username'";
+    $result = mysqli_query($con,$sql);
+
+    if ($result){
+        $num = mysqli_num_rows($result);
+        if ($num>0) {
+            // echo "User already exists";
+            $user=1;
+        } else {
+            $sql = "insert into `registration` (username, password) values('$username', '$password')";
+            $result=mysqli_query($con,$sql);
+                
+            if ($result) {
+                // echo "Sign up succesful";
+                $success=1;
+                header('location:login.php');
+            } else {
+                die(mysqli_error($con));
+            }
+        }
+    }
+}
+
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Signup Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+
+  <?php
+    if($user){
+        echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Sorry</strong> user already exists
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>';
+    }
+
+    ?>
+
+<?php
+    if($success){
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success</strong> you are signed up.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>';
+    }
+
+    ?>
+    <h1 class="text-center">Sign up Page</h1>
+    <div class="container mt-5">
+        <form action="sign.php" method="post">
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Name</label>
+        <input type="text" class="form-control" placeholder="Enter your username" name="username">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" placeholder="Enter your password" name="password">
+    </div>
+    <button type="submit" class="btn btn-primary w-100">Sign up</button>
+    </form>
+    </div>
+  </body>
+</html>
